951bf671d61082a5d70dda4faf579dbdcc81441d
thewardoctor <email@email.com>
2023-03-09 14:57:10 +1000

    fix textbook error

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,12 @@ fn main() {
     // 'incoming' returns an iterator over the received connections
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {
+            Ok(mut stream) => {
                 if let Err(_) = stream.write(b"hello world") {
                     println!("Failed to write hello to stream");
                 }
             }
-            Err(e) => {
+            Err(_) => {
                 println!("This stream's connection has failed");
             }
         }
